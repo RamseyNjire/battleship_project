5d66b6936702bc26b4f10e1ac40ff4b647d4292b
Ramsey Njire <ramseynjire@gmail.com>
2021-10-26 20:59:14 +0300

    write board#attack method

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,32 @@
 class Board
     attr_reader :size
-  def initialize(length)
-    @grid = Array.new(length){ Array.new(length, :N) }
-    @size = length * length
-  end
-
-  def [](position)
-    @grid[position[0]][position[1]]
-  end
-
-  def []=(position, value)
-    @grid[position[0]][position[1]] = value
-  end
-
-  def num_ships
-    @grid.reduce(0) do |sum, array|
-        sum + array.count{ |element| element == :S }
+    def initialize(length)
+        @grid = Array.new(length){ Array.new(length, :N) }
+        @size = length * length
     end
-  end
+
+    def [](position)
+        @grid[position[0]][position[1]]
+    end
+
+    def []=(position, value)
+        @grid[position[0]][position[1]] = value
+    end
+
+    def num_ships
+        @grid.reduce(0) do |sum, array|
+            sum + array.count{ |element| element == :S }
+        end
+    end
+
+    def attack(position)
+        if self[position] == :S
+            self[position] = :H
+            puts "You sunk my battleship!"
+            return true
+        else
+            self[position] = :X
+            false
+        end
+    end
 end
