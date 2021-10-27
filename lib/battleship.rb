10760585fe186afcc0c716d4eb4bc91d555a91c0
Ramsey Njire <ramseynjire@gmail.com>
2021-10-27 20:56:35 +0300

    write battleship#initialize method

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -2,5 +2,9 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-
+    def initialize(length)
+        @player = Player.new
+        @board = Board.new(length)
+        @remaining_misses = @board.size / 2
+    end
 end
