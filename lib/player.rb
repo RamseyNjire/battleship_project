c3ad2373238b7396c11fcea7d9b3812d305b6bf5
Ramsey Njire <ramseynjire@gmail.com>
2021-10-27 20:31:34 +0300

    write player#get_move method

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,6 @@
 class Player
-
+    def get_move
+        puts "Enter a position with coordinates separated by a space, like '4 7'"
+        position = gets.chomp.split(" ").map{ |element| element.to_i }
+    end
 end
