18f43bf8241d97eb17ac2f09994d56cf37ff0ebb
Ramsey Njire <ramseynjire@gmail.com>
2021-10-27 21:08:55 +0300

    write battleship#start_game method and add getter for @board instance variable in battleship class

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -2,10 +2,16 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-    attr_reader :player
+    attr_reader :player, :board
     def initialize(length)
         @player = Player.new
         @board = Board.new(length)
         @remaining_misses = @board.size / 2
+    end
+
+    def start_game
+        @board.place_random_ships
+        puts "There are #{@board.num_ships} ships on the board"
+        @board.print
     end
 end
